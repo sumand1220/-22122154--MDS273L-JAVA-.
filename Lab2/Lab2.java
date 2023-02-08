48d41822e319324cd21d5d793e1f8ee094601735
sumand1220 <118504682+sumand1220@users.noreply.github.com>
2023-02-08 19:29:12 +0530

    Update Lab2.java

--- Lab2/Lab2.java
+++ Lab2/Lab2.java
@@ -1,119 +1,91 @@
 import java.util.Scanner;
 
 public class Lab2 {
-  private static final int MAX_NAMES = 1024;
-  private static String[] names = new String[MAX_NAMES];
-  private static int numNames = 0;
+  static String[] names = new String[1024];
+  static int nameCount = 0;
 
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int option;
+    Scanner scanner = new Scanner(System.in);
+    int choice;
 
     do {
-      System.out.println("--- Menu ---");
+      System.out.println("\nMenu");
       System.out.println("1. Enter a name");
       System.out.println("2. Search for a name");
       System.out.println("3. Remove a name");
       System.out.println("4. Show all names");
       System.out.println("5. Exit");
-      System.out.print("Enter your option: ");
+      System.out.print("Enter your choice: ");
+      choice = scanner.nextInt();
+      scanner.nextLine();
 
-      option = sc.nextInt();
-      sc.nextLine();
-
-      switch (option) {
+      switch (choice) {
         case 1:
-          enterName(sc);
+          System.out.print("Enter a name: ");
+          names[nameCount] = scanner.nextLine();
+          nameCount += 1;
+          System.out.println("Name added successfully");
           break;
         case 2:
-          searchName(sc);
+          System.out.print("Enter a name to search: ");
+          String searchName = scanner.nextLine();
+
+          boolean c_name = false; // checking if the name to search is present
+          int index = -1;
+          for (int i = 0; i < nameCount; i++) {
+            if (names[i].equals(searchName)) {
+              c_name = true;
+              index = i;
+              break;
+            }
+          }
+
+          if (c_name) {
+            System.out.println("Name found at index: " + index);
+          } else {
+            System.out.println("Name not found");
+          }
           break;
         case 3:
-          removeName(sc);
+          System.out.print("Enter a name to remove: ");
+          String removeName = scanner.nextLine();
+
+          c_name = false;   // checking if the name to remove is present
+          index = -1;
+          for (int i = 0; i < nameCount; i++) {
+            if (names[i].equals(removeName)) {
+              c_name = true;
+              index = i;
+              break;
+            }
+          }
+
+          if (c_name) {
+            for (int i = index; i < nameCount - 1; i++) {
+              names[i] = names[i + 1];
+            }
+            nameCount--;
+            System.out.println("Name removed successfully");
+          } else {
+            System.out.println("Name not found");
+          }
           break;
         case 4:
-          showNames();
+          if (nameCount == 0) {
+            System.out.println("No names to show");
+          } else {
+            System.out.println("All names: ");
+            for (int i = 0; i < nameCount; i++) {
+              System.out.println(names[i]);
+            }
+          }
           break;
         case 5:
-          System.out.println("Exiting program...");
+          System.out.println("Exiting program");
           break;
         default:
-          System.out.println("Invalid option, try again");
-      }
-    } while (option != 5);
-
-    sc.close();
-  }
-
-  private static void enterName(Scanner sc) {
-    System.out.print("Enter a name: ");
-    String name = sc.nextLine();
-
-    if (name.isEmpty() || numNames == MAX_NAMES) {
-      System.out.println("Invalid name or maximum limit reached");
-      return;
-    }
-
-    for (int i = 0; i < numNames; i++) {
-      if (names[i].equalsIgnoreCase(name)) {
-        System.out.println("Name already exists");
-        return;
-      }
-    }
-
-    names[numNames++] = name;
-    System.out.println("Name added successfully");
-  }
-
-  private static void searchName(Scanner sc) {
-    System.out.print("Enter a name to search: ");
-    String search = sc.nextLine();
-
-    boolean found = false;
-    for (int i = 0; i < numNames; i++) {
-      if (names[i].equalsIgnoreCase(search)) {
-        System.out.println("Name found at index " + i);
-        found = true;
-        break;
-      }
-    }
-
-    if (!found) {
-      System.out.println("Name not found");
+          System.out.println("Invalid choice. Try again");
+        }
+      } while (choice != 5);
     }
   }
-
-  private static void removeName(Scanner sc) {
-    System.out.print("Enter a name to remove: ");
-    String remove = sc.nextLine();
-
-    int index = -1;
-    for (int i = 0; i < numNames; i++) {
-      if (names[i].equalsIgnoreCase(remove)) {
-        index = i;
-        break;
-      }
-    }
-
-    if (index == -1) {
-      System.out.println("Name not found");
-    } else {
-        for (int i = index; i < numNames - 1; i++) {
-        names[i] = names[i + 1];
-        }
-        numNames--;
-        System.out.println("Name removed successfully");
-        }
-        }
-        
-        private static void showNames() {
-        if (numNames == 0) {
-        System.out.println("No names to show");
-        return;
-        }
-        System.out.println("--- Names ---");
-for (int i = 0; i < numNames; i++) {
-  System.out.println((i + 1) + ". " + names[i]);
-}
-        }
-    }
