5693f80620ec43a12b109e244ad9ee89e92ba6c3
sumand1220 <suman.d@msds.christuniversity.in>
2023-02-15 12:07:29 +0530

    Lab

--- Lab2/Lab2.java
+++ Lab2/Lab2.java
@@ -1,119 +1,58 @@
-import java.util.Scanner;
-
+import java.util.*;
 public class Lab2 {
-  private static final int MAX_NAMES = 1024;
-  private static String[] names = new String[MAX_NAMES];
-  private static int numNames = 0;
+  public static void main(String[] args){
+    scanner scan = new Scanner(System.in);
+    String arr[] = new String[1024];
+    int arrLen = 0;
+    int loopControl = 0;
 
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int option;
+    do{
+      //printing the menu option
+      Sysyem.out.println("Menu OPtions.\n1.enter name.\n2.List alll\n.3.Remove name\n.4Search\n.5Exit")
+      int userChoice=Integer.parsInt(scan.nextLine());
+      switch(userchoice){
+        case1:
+            System.out.println("Enter the new name: ");
+            String tempName = scan.nextLine();
+            boolean addName= false;
 
-    do {
-      System.out.println("--- Menu ---");
-      System.out.println("1. Enter a name");
-      System.out.println("2. Search for a name");
-      System.out.println("3. Remove a name");
-      System.out.println("4. Show all names");
-      System.out.println("5. Exit");
-      System.out.print("Enter your option: ");
+            if(arrLen == 0){
+              addName = true;
+            }else{
+              for(int i=0;i<arrLen;i++){
+                if(arr[i].equalsIgnoreCase(tempName)){
+                    addName = flase;
 
-      option = sc.nextInt();
-      sc.nextLine();
+                }else{
+                    addName = true;
+                }
+              }
+            }
 
-      switch (option) {
-        case 1:
-          enterName(sc);
-          break;
-        case 2:
-          searchName(sc);
-          break;
-        case 3:
-          removeName(sc);
-          break;
-        case 4:
-          showNames();
-          break;
-        case 5:
-          System.out.println("Exiting program...");
-          break;
+            if(addName == true){
+              arr[arrLen] = tempName;
+              arrLen = arrLen + 1;
+              System.out.println("Name Added");
+
+            }else{
+                System.out.println("Name existes..");
+            }
+              break;
+        case2:
+            for(int i=0;i<arrLen;i++){}
+              break;
+        case3:
+              break;
+        case4:
+              break;
         default:
-          System.out.println("Invalid option, try again");
+              System.out.println("Wrong Choice");
       }
-    } while (option != 5);
+      System.out.println("Press 1 to continue ....!");
+      loopControl = Integer.parseInt(scan.nextLine());
 
-    sc.close();
+    }while(loopControl == 1);
+
+    System.out.println("Program Exits")
   }
-
-  private static void enterName(Scanner sc) {
-    System.out.print("Enter a name: ");
-    String name = sc.nextLine();
-
-    if (name.isEmpty() || numNames == MAX_NAMES) {
-      System.out.println("Invalid name or maximum limit reached");
-      return;
-    }
-
-    for (int i = 0; i < numNames; i++) {
-      if (names[i].equalsIgnoreCase(name)) {
-        System.out.println("Name already exists");
-        return;
-      }
-    }
-
-    names[numNames++] = name;
-    System.out.println("Name added successfully");
-  }
-
-  private static void searchName(Scanner sc) {
-    System.out.print("Enter a name to search: ");
-    String search = sc.nextLine();
-
-    boolean found = false;
-    for (int i = 0; i < numNames; i++) {
-      if (names[i].equalsIgnoreCase(search)) {
-        System.out.println("Name found at index " + i);
-        found = true;
-        break;
-      }
-    }
-
-    if (!found) {
-      System.out.println("Name not found");
-    }
-  }
-
-  private static void removeName(Scanner sc) {
-    System.out.print("Enter a name to remove: ");
-    String remove = sc.nextLine();
-
-    int index = -1;
-    for (int i = 0; i < numNames; i++) {
-      if (names[i].equalsIgnoreCase(remove)) {
-        index = i;
-        break;
-      }
-    }
-
-    if (index == -1) {
-      System.out.println("Name not found");
-    } else {
-        for (int i = index; i < numNames - 1; i++) {
-        names[i] = names[i + 1];
-        }
-        numNames--;
-        System.out.println("Name removed successfully");
-        }
-        }
-        
-        private static void showNames() {
-        if (numNames == 0) {
-        System.out.println("No names to show");
-        return;
-        }
-        System.out.println("--- Names ---");
-for (int i = 0; i < numNames; i++) {
-  System.out.println((i + 1) + ". " + names[i]);
 }
-        }
-    }
